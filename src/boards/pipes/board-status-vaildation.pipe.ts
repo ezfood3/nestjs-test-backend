7a513fc430042166de9dc72e4e11f243a3545c27
root <root@DESKTOP-LJBEIFA>
2023-12-05 01:42:48 +0900

    update: board-status-vaildation pipe

---
+++ src/boards/pipes/board-status-vaildation.pipe.ts
@@ -0,0 +1,24 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { BoardStatus } from "../board.model";
+
+export class BoardStatusVaildationPipe implements PipeTransform {
+  readonly StatusOption = [
+    BoardStatus.PUBLIC,
+    BoardStatus.PRIVATE,
+  ]
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValue(value)) {
+      throw new BadRequestException(`${value} ins't in the status options`);
+    }
+
+    return value;
+  }
+
+  private isStatusValue(status: any) {
+    const index = this.StatusOption.indexOf(status);
+    return index !== -1
+  }
+}
